3fa46cef068aa389a83a69a831915e831b7a0038
Ethan Hedrick <ethan@sneakysemicolon.com>
2015-04-30 01:07:22 -0400

    beginnings of adding methods to the "pages". Only adding to see if travis is fixed.

--- lib/puppis.rb
+++ lib/puppis.rb
@@ -4,6 +4,15 @@ require_relative 'puppis/config'
 
 module Puppis
   def self.included(base)
-    # base.extend(something)
+    classes = Puppis::Elements.constants.select { |c| Puppis::Elements.const_get(c).is_a? Class }
+
+    classes.each do |klass|
+      klass.actions.each do |action_name, func|
+        base.send(:define_method, action_name) do |*args|
+          func.call(*[klass.new, *args])
+        end
+      end
+    end
+
   end
 end
